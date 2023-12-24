a99188ca8096ca4643ab01f2428ca3e9d53e70e4
alexleelab <alexleelab15@gmail.com>
2023-12-24 19:35:47 +0800

    add readme, requirements, modify for simplicity

--- env_test.py
+++ env_test.py
@@ -1,40 +1,82 @@
+import warnings
 import numpy as np
 import gymnasium as gym
 from gymnasium.envs.registration import register
 from stable_baselines3 import PPO, A2C, DQN
-from transformation_based import output2gates
-from utils import string2index
+from transformation_based import output2gates_basic, output2gates_bidirectional
+from utils import string2index, make_test
+
+from sb3_contrib.common.wrappers import ActionMasker
+from sb3_contrib.ppo_mask import MaskablePPO
+from sb3_contrib.common.maskable.utils import get_action_masks
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+warnings.filterwarnings("ignore")
 
 register(
     id='rls-eval',
     entry_point='envs:RLS_eval'
 )
 
-model_path = "models/best"
-env = gym.make('rls-eval')
-model = PPO.load(model_path, env=env)
-episodes = 1000
-n = 3
-list2id = string2index(n)
+my_config = {
+    # select different algorithms: PPO, A2C, DQN, MaskablePPO
+    "algorithm": MaskablePPO,
+    "model_path" : "models/best",
 
-ours, algorithms = 0, 0
-for i in range(episodes):
+    "n" : 4,
+    "eval_episodes" : 1000
+}
+
+def mask_fn(env:gym.Env) -> np.ndarray:
+    return env.valid_action_mask()
+
+def make_env(seed=None, **kwargs):
+    env = gym.make('rls-eval')
+    if my_config["algorithm"] == MaskablePPO:
+        env = ActionMasker(env,mask_fn)
+    return env
+
+env = make_env()
+model = my_config["algorithm"].load(my_config["model_path"], env=env)
+
+list2id = string2index(my_config["n"])
+tests = make_test(my_config["n"], my_config["eval_episodes"], 1002)
+
+ours, fail = 0, 0
+b_cnt, sb_cnt = 0, 0
+for i in range(my_config["eval_episodes"]):
     done = False
     obs, info = env.reset()
+    obs[1] = np.array(tests[i])
     intlist = [list2id[''.join([str(xx) for xx in x.tolist()])] for x in obs[1].astype(int)]
-    baseline = len(output2gates(3, intlist))
+    baseline = len(output2gates_basic(my_config["n"], intlist.copy()))
+    strong_baseline = len(output2gates_bidirectional(my_config["n"], intlist.copy()))
     # print(obs)
     cnt = 0
     while not done:
-        action, _ = model.predict(obs, deterministic=True)
+        if my_config["algorithm"] == MaskablePPO:
+            action_masks = get_action_masks(env)
+            action, _ = model.predict(obs, deterministic=True, action_masks=action_masks)
+        else:
+            action, _ = model.predict(obs, deterministic=True)
         obs, r, done, _, info = env.step(action)
         cnt += 1
-    if info["MatchCnt"][-1] != 8:
-        print(info["MatchCnt"][-1], cnt)
-    ours += cnt
-    algorithms += baseline
+    if info["MatchCnt"][-1] != 1 << my_config["n"]:
+        #print(info["MatchCnt"][-1], cnt)
+        fail += 1
+    else:
+        ours += cnt
+        b_cnt += baseline
+        sb_cnt += strong_baseline
 
     # modify = # of legal moves in this episode
     # matchcnt = # of matched permutations
     # print(f'Episode: {i+1}, MatchCnt: {info["MatchCnt"][-1]}, Gate: {cnt}, Baseline: {baseline}')
-print(f'our steps = {ours / episodes} \n transformation base = {algorithms / episodes}')
+        
+if my_config["eval_episodes"] - fail == 0 :
+    print(f'the model in {my_config["model_path"]} fail all test')
+else :
+    print(f'our steps = {ours / (my_config["eval_episodes"] - fail)} \n',
+            f'our correct rate = {(my_config["eval_episodes"] - fail) / my_config["eval_episodes"]}\n',
+            f'base = {b_cnt / (my_config["eval_episodes"] - fail)} \n',
+            f'bidirectional = {sb_cnt / (my_config["eval_episodes"] - fail)}')
--- train.py
+++ train.py
@@ -2,41 +2,56 @@ import warnings
 import gymnasium as gym
 from gymnasium.envs.registration import register
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
+from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder, VecEnv
 from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC, TD3
 import torch
 import torch.nn as nn
 from gymnasium import spaces
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 import numpy as np
+from utils import make_test
+
+from sb3_contrib.common.maskable.policies import MaskableActorCriticPolicy
+from sb3_contrib.common.wrappers import ActionMasker
+from sb3_contrib.ppo_mask import MaskablePPO
+from sb3_contrib.common.maskable.utils import get_action_masks
 
 warnings.filterwarnings("ignore")
 
 register(
     id='rls-v0',
-    entry_point='envs:RLS'
+    entry_point='envs:RLS_eval'
 )
 
 # Set hyper params (configurations) for training
 my_config = {
     "run_id": "example",
-
-    "algorithm": PPO,
+    # select different algorithms: PPO, A2C, DQN, MaskablePPO
+    "algorithm": MaskablePPO,
+    "model_path" : "models/best",
     "policy_network": "MlpPolicy",
 
-    "epoch_num": 1000,
-    "timesteps_per_epoch": 5000,
-    "eval_episode_num": 10,
-}
+    "n":3,
 
-def make_env():
+    "epoch_num": 500,
+    "timesteps_per_epoch": 500,
+    "eval_episode_num": 100,
+}
+def mask_fn(env:gym.Env) -> np.ndarray:
+    return env.valid_action_mask()
+
+def make_env(seed=None, **kwargs):
     env = gym.make('rls-v0')
+    if my_config["algorithm"] == MaskablePPO:
+        env = ActionMasker(env,mask_fn)
     return env
 
 def train(env, model, config):
 
     current_best = -10000000
     matched_best = 0
+    eval_epochs = config["eval_episode_num"]
+    tests = make_test(config["n"], eval_epochs, 5487) # select a particular seed for evaluation
 
     for epoch in range(config["epoch_num"]):
 
@@ -45,60 +60,55 @@ def train(env, model, config):
         model.learn(
             total_timesteps=config["timesteps_per_epoch"],
             reset_num_timesteps=False,
-            # callback=WandbCallback(
-            #     gradient_save_freq=100,
-            #     verbose=2,
-            # ),
         )
 
         ### Evaluation
         print("Epoch: ", epoch)
         total_score = 0
         matched = 0
-        for i in range(20):
+        for i in range(eval_epochs):
             done = False
             score = 0
             obs = env.reset()
+            obs[0][1] = tests[i]
             cnt = 0
+            temp = []
             while not done:
-                action, _ = model.predict(obs, deterministic=True)
+                if config["algorithm"] == MaskablePPO:
+                    action_masks = get_action_masks(env)
+                    # if don't apply action mask during eval, in the eval action wouldn't be masked
+                    action, _ = model.predict(obs, deterministic=True, action_masks=action_masks)
+                else:
+                    action, _ = model.predict(obs, deterministic=True)
+
                 obs, r, done, info = env.step(action)
                 score += r[0]
+                temp.append(r[0])
                 cnt += 1
             total_score += score
             matched += info[0]["MatchCnt"][-1]
             # modify = # of legal moves in this episode
             # matchcnt = # of matched permutations
-            print(f'Episode: {i+1}, Score: {score}, Gate: {cnt}, MatchCnt: {info[0]["MatchCnt"][-1]}, Modify: {info[0]["Modify"]}, \n Initial State: \n {np.transpose(info[0]["Initial_State"])}, \n End State: \n {np.transpose(info[0]["End_State"])}')
-            gates = info[0]["GateTrace"]
-            for ele in np.transpose(gates):
-                print(ele)
+            # print(temp)
+            # print(f'Episode: {i+1}, Score: {score}, Gate: {cnt}, MatchCnt: {info[0]["MatchCnt"][-1]}, Modify: {info[0]["Modify"]}, \n Initial State: \n {np.transpose(info[0]["Initial_State"])}, \n End State: \n {np.transpose(info[0]["End_State"])}')
+            # gates = info[0]["GateTrace"]
+            # for ele in np.transpose(gates):
+            #     print(ele)
             
-        print(f'the average score is {total_score / 20}')
+        print(f'the average score is {total_score / eval_epochs}')
         ### Save best model
-        if current_best < total_score and matched >= matched_best:
-            print(f"Saving Model with avg score {total_score / 20} and avg matchCnt {matched / 20}")
+        if current_best <= total_score and matched >= matched_best:
+            print(f"Saving Model with avg score {total_score / eval_epochs} and avg matchCnt {matched / eval_epochs}")
             current_best = total_score
             matched_best = matched
-            save_path = 'models'
-            model.save(f"{save_path}/best")
+            model.save(config["model_path"])
 
         print("---------------")
 
-        print(current_best / 20)
-        print(matched_best / 20)
-
+        print(current_best / eval_epochs)
+        print(matched_best / eval_epochs)
 
 if __name__ == "__main__":
-
-    # Create wandb session (Uncomment to enable wandb logging)
-    # run = wandb.init(
-    #     project="assignment_3",
-    #     config=my_config,
-    #     sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
-    #     id=my_config["run_id"]
-    # )
-
     env = DummyVecEnv([make_env])
 
     # Create model from loaded config and train
@@ -108,6 +118,6 @@ if __name__ == "__main__":
         env,
         verbose=0,
         learning_rate=0.0003,
-        #tensorboard_log=my_config["run_id"]
+        # tensorboard_log=my_config["run_id"]
     )
     train(env, model, my_config)
